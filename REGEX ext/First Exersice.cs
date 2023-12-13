704a38f3a83e0bbf930998fb26b0ecf23590b7bb
Bogdan Sadovskiy <bohdansadovskiy@gmail.com>
2023-12-13 19:56:59 +0200

    Add project files.

---
+++ REGEX ext/First Exersice.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace mySpace
+{
+    internal class Exersice
+    {
+
+        public void IsEmailValid()
+        {
+            // Is Email valid
+            while (true)
+            {
+                Console.WriteLine("Input Email:");
+                Console.WriteLine("Email has not got #,=,+; Has only one @");
+                Console.WriteLine("E - exit");
+
+                string Email = Console.ReadLine();
+                if (Email == "e" || Email == "E") { return; }
+                var Email_ = Regex.Match(Email, "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+                if (Email_.Success)
+                {
+                    Console.WriteLine("Valided");
+                }
+                else { Console.WriteLine("Not valided"); }
+            }
+        }
+
+        public void IsUkrainianNumberValid()
+        {
+            while (true)
+            {
+                Console.WriteLine("Input number:");
+                Console.WriteLine("Number must be start from +380");
+                Console.WriteLine("E - exit");
+
+                string str = Console.ReadLine();
+                if (str == "e" || str == "E") { return; }
+                var UKR = Regex.Match(str, "^\\+(380)[\\d]{9}$");
+                if (UKR.Success)
+                {
+                    Console.WriteLine("Number is valid");
+                }
+                else { Console.WriteLine("Number is not valid"); }
+
+            }
+        }
+    }
+}
---
+++ REGEX ext/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace mySpace;
+
+class programm
+{
+    static public void Main(string[] args)
+    {
+        Exersice exersice = new Exersice();
+        Console.WriteLine("Is Email valid ------------ 1");
+        Console.WriteLine("Is Ukrainian number valid - 2");
+
+        string menu = Console.ReadLine();
+        int menu_= 0;
+        try
+        {
+            menu_ = Int32.Parse(menu);
+        }
+        catch { Console.WriteLine("Wrong input"); }
+        Console.Clear();   
+        if(menu_ == 1) {
+            exersice.IsEmailValid();
+        }
+        else if (menu_ == 2)
+        {
+            exersice.IsUkrainianNumberValid();
+        }
+
+    }
+}
